74c9888f04f5e4396d5ab0783fcdd3e46883b3a2
Aperlop2 <anita47926@gmail.com>
2026-09-22 16:03:33 -0600

    entrega de calculadora basica javascript

---
+++ CALCULADORA-JS_LAB6/calculadora.js
@@ -0,0 +1,15 @@
+
+let numero1 = Number(prompt("ingresa el primer numero: "));
+let numero2 = Number(prompt("ingresa otro numero: "));
+let nombre = prompt("como te llamas?");
+console.log("hola ", nombre, " estos son tus resultados:");
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+
+console.log("la suma es:", suma);
+console.log("la resta es: ", resta);
+console.log("la multiplicacion es: ", multiplicacion);
+console.log("la division es: ", division);
