b6cc79ac95366155f71efede6ec391c7953b1685
Aperlop2 <anita47926@gmail.com>
2026-09-22 18:58:30 -0600

    entrega de bucles

---
+++ LAB7_CODIFICACION_DE_BUCLES/sd-l1-1.0/task1.js
@@ -0,0 +1,5 @@
+// Refer to Task 1 in your Instructions to complete this task
+//MOSTRAR TODOS LOS NUMEROS DEL 1 AL 105
+for (let i = 1; i <= 105; i++) {
+    console.log(i);
+}
---
+++ LAB7_CODIFICACION_DE_BUCLES/sd-l1-1.0/task5.js
@@ -0,0 +1,10 @@
+// Refer to Task 5 in your Instructions to complete this task
+//indicar al usuario que introduzca el numero de lineas
+// que se generarran o que genere una en especifica
+let numLines = Number(prompt("inserte el numero de lineas para generar"));
+for (let i = 1; i <= numLines; i++){
+  for (let j = 1; j <= i; j++){
+    console.log("-");
+  }
+  console.log("\n");
+}
